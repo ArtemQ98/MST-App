157c4e38d87c5925e5ea50d6d3ac4b7fb72fa7aa
Artem Kurochkin <ArtemQ98@yandex.ru>
2025-12-15 21:53:01 +0300

    Initial Commit

---
+++ MST_APP/MST_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MST_APPApp.swift
+//  MST_APP
+//
+//  Created by Артём Курочкин on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MST_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
